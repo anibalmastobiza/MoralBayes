9cb96f38e911ea3f0fd35b3121c3f74eb6a3029a
Aníbal M. Astobiza <32415432+anibalmastobiza@users.noreply.github.com>
2025-06-05 12:26:22 +0200

    Update exp.js

--- experiment/exp.js
+++ experiment/exp.js
@@ -1,1 +1,20 @@
+(async () => {
+  const jsPsych = initJsPsych({on_finish: () => jsPsych.data.displayData()});
 
+  const dilemmas = await fetch("../stimuli/dilemmas.json").then(r => r.json());
+
+  // Oxford Utilitarianism Scale placeholder
+  const ous_items = [...Array(10).keys()].map(i => ({
+     prompt: `Utilitarian item ${i+1}`, labels: ["Strongly disagree","...","Strongly agree"], required:true
+  }));
+  const ous = {type: jsPsychSurveyLikert, questions: ous_items, preamble:"Oxford Utilitarianism Scale"};
+
+  // Trials
+  const trials = dilemmas.map(d => ({
+     type: jsPsychHtmlSliderResponse,
+     stimulus: `<p>${d.scenario}</p><p><em>${d.arguments.util_clear}</em></p>`,
+     labels:["0% morally permissible","100% morally permissible"]
+  }));
+
+  jsPsych.run([ous, ...trials]);
+})();
